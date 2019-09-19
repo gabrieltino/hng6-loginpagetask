8c6643487967705b73ab078732e5585adf0ac79b
gabrieltino <miracle8093@gmail.com>
2019-09-19 21:35:46 +0100

    redirect after registration

--- config.php
+++ config.php
@@ -3,15 +3,15 @@
 // session_start();
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '1smartcoded');
-define('DB_NAME', 'hng');
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '1smartcoded');
+// define('DB_NAME', 'hng');
 
-// define('DB_SERVER', 'db4free.net:3306');
-// define('DB_USERNAME', 'teamsixpathdev');
-// define('DB_PASSWORD', 'sixpathdev');
-// define('DB_NAME', 'teamsixpathdev');
+define('DB_SERVER', 'db4free.net:3306');
+define('DB_USERNAME', 'teamsixpathdev');
+define('DB_PASSWORD', 'sixpathdev');
+define('DB_NAME', 'teamsixpathdev');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
